29c49222a96c847ac5a57f85745be5bd2c475877
Lars Smith Eier <lars.smith.eier@basefarm-orange.com>
2024-03-31 16:30:59 +0200

    asdasd

--- modules/editor/default.nix
+++ modules/editor/default.nix
@@ -9,7 +9,7 @@ in
     program = lib.mkOption { type = lib.types.enum [ "neovim" "vim" ]; default = "vim"; };
   };
 
-  config = lib.mkIf cfg.enable lib.mkMerge ([
+  config = lib.mkIf cfg.enable (lib.mkMerge [
     (lib.mkIf (cfg.editor == "neovim") (import ./editors/neovim.nix))
     (lib.mkIf (cfg.editor == "vim") (import ./editors/vim.nix))
   ]);

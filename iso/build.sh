12a62963d6e526918006085e433c09ea6e3db5e7
Lars Smith Eier <lars.smith.eier@basefarm-orange.com>
2024-03-31 01:34:50 +0100

    small tweaks

--- iso/build.sh
+++ iso/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 script_location="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
+
 nix build ${script_location}#nixosConfigurations.minimal_iso.config.system.build.isoImage

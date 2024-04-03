54373234629c7c4d7956e7e6cc0610b914d4b735
Lars Smith Eier <lars.smith.eier@basefarm-orange.com>
2024-04-03 21:15:44 +0200

    asdasd

--- modules/boot_loader/grub.nix
+++ modules/boot_loader/grub.nix
@@ -8,9 +8,11 @@
       useOSProber = cfg.grub.useOSProber;
       theme = pkgs.stdenv.mkDerivation {
         name = "grub_theme";
-        src = builtins.fetchGit {
-          url = "https://github.com/AdisonCavani/distro-grub-themes.git";
-          ref = "master";
+        src = pkgs.fetchFromGitHub {
+          owner = "AdisonCavani";
+          repo = "distro-grub-themes";
+          rev = "c96f868e75707ea2b2eb2869a3d67bd9c151cee6";
+          hash = "sha256-QHqsQUEYxa04je9r4FbOJn2FqRlTdBLyvwZXw9JxWlQ=";
         };
         installPhase = "tar -xf themes/nixos.tar -C $out";
       };

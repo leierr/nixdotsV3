2d65342060888dd7de4a750b4022bd6862401851
Lars Smith Eier <lars.smith.eier@basefarm-orange.com>
2024-06-22 14:40:30 +0200

    asdasd

--- iso/build.sh
+++ iso/build.sh
@@ -3,5 +3,4 @@
 set -euo pipefail
 script_location="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 
-nix flake update --no-write-lock-file "${script_location}"
-nix build --no-link --print-out-paths --cores 0 ${script_location}#nixosConfigurations.minimal_iso.config.system.build.isoImage | xargs -I{} find {} -type f -name "*.iso"
+nix build --no-link --print-out-paths --no-write-lock-file --cores 0 ${script_location}#nixosConfigurations.minimal_iso.config.system.build.isoImage | xargs -I{} find {} -type f -name "*.iso"

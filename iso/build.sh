b7e9b300934118ad6e65826aa43d8c0adae2edcb
Lars Smith Eier <lars.smith.eier@basefarm-orange.com>
2024-06-22 14:30:14 +0200

    fuck me

--- iso/build.sh
+++ iso/build.sh
@@ -3,5 +3,5 @@
 set -euo pipefail
 script_location="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 
-nix flake update "${script_location}"
+nix flake update --no-write-lock-file "${script_location}"
 nix build --no-link --print-out-paths --cores 0 ${script_location}#nixosConfigurations.minimal_iso.config.system.build.isoImage | xargs -I{} find {} -type f -name "*.iso"

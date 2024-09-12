97c32cbcf1cb507c85f7fb02f477e82974d81e42
Lars Smith Eier <hBm5BEqULhwPKUY@protonmail.com>
2024-09-12 20:28:56 +0200

    asdasdas

--- flake.nix
+++ flake.nix
@@ -34,7 +34,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/release-24.05";
     nixpkgs_unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     #
-    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 }

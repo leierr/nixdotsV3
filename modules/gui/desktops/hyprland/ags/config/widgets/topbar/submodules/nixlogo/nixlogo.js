f08a8464338ea86e6f4ea7bffe633d237f952b21
Lars Smith Eier <hBm5BEqULhwPKUY@protonmail.com>
2024-09-23 21:03:33 +0200

    asdasd

--- modules/gui/desktops/hyprland/ags/config/widgets/topbar/submodules/nixlogo/nixlogo.js
+++ modules/gui/desktops/hyprland/ags/config/widgets/topbar/submodules/nixlogo/nixlogo.js
@@ -1,7 +1,7 @@
 export default () => Widget.EventBox({
     class_name: 'nixlogo_widget',
     child: Widget.Icon({
-        icon: `${App.configDir}/assets/nixsnowflake_mono.svg`,
+        icon: `${App.configDir}/assets/nixsnowflake.svg`,
         class_name: 'nixlogo_icon'
     })
 })
